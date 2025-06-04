f03d0ee53c7615fdc514a49de3acf312e72f3b48
emilmagdy <dremilmagdy@gmail.com>
2025-06-04 22:44:55 +0300

    A

--- src/command_exit.ts
+++ src/command_exit.ts
@@ -1,7 +1,9 @@
+import { PokeAPI } from "./pokeapi.js";
 import { type State } from "./state.js"
 
 export async function commandExit(state: State) {
     console.log("Closing the Pokedex... Goodbye!");
     state.readline.close();
+    state.pokeAPI.stopCache()
     process.exit();
 };
